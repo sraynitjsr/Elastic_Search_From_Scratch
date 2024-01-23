3855864376ed490a55ed58b455e8da303d40c455
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-23 23:04:33 +0530

    Search and Delete Documents Implementation

--- Golang_Implementation/cmd/main.go
+++ Golang_Implementation/cmd/main.go
@@ -20,7 +20,8 @@ func main() {
 
 	indexDocument(es)
 	retrieveDocument(es, "1")
-
+	searchDocuments(es)
+	deleteDocument(es, "1")
 }
 
 func indexDocument(es *elasticsearch.Client) {
@@ -46,4 +47,28 @@ func retrieveDocument(es *elasticsearch.Client, documentID string) {
 	defer getResponse.Body.Close()
 
 	fmt.Printf("Retrieved document: %s\n", getResponse.String())
+}
+
+func searchDocuments(es *elasticsearch.Client) {
+	searchResponse, err := es.Search(
+		es.Search.WithIndex("my_index_name"),
+		es.Search.WithBody(strings.NewReader(`{"query": {"match_all": {}}}`)),
+	)
+	if err != nil {
+		log.Fatalf("Error searching documents: %s", err)
+	}
+	defer searchResponse.Body.Close()
+
+	fmt.Println("Search results:")
+	fmt.Println(searchResponse.String())
+}
+
+func deleteDocument(es *elasticsearch.Client, documentID string) {
+	deleteResponse, err := es.Delete("my_index_name", documentID)
+	if err != nil {
+		log.Fatalf("Error deleting document: %s", err)
+	}
+	defer deleteResponse.Body.Close()
+
+	fmt.Printf("Document deleted: %s\n", deleteResponse.String())
 }
